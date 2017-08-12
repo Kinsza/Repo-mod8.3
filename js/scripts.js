1020605b7074b073ec4455e803bfe11e7df88b7f
Kinsza <code1@onet.pl>
2017-08-12 22:32:56 +0200

    added new comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // scripts.js
 
 var name = prompt('Enter your name');
+// Learns to insert a new comment
 alert('Hello, ' + name);
+// Everything works so no more comments are needed
 console.log('Hello, ' + name);
