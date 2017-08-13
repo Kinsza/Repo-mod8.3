8e49feb86755a9e0db8db66d141640343cd8bbfe
Kinsza <code1@onet.pl>
2017-08-14 00:16:40 +0200

    Added new task

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ alert('Hello, ' + name);
 // Everything works so no more comments are needed
 console.log('Hello, ' + name);
 
-//You want to check the next task, remove '/*' '*/'
+//You want to check the next task, remove '/* ' '*/ '
 
 //New task - Mod 8.5
 /*
@@ -19,4 +19,34 @@ var triangleArea = a*h/2;
 alert('wynik, ' + triangleArea);
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+*/
+
+//New Task - Mod 8.6
+/*
+var a = prompt ('a');
+var b = prompt ('b');
+var value = (a*a)+(2*a*b)-(b*b);
+
+if (value > 0) {
+    alert('Wynik dodatni , ' + value)
+} else if (value < 0) {
+    alert('Wynik ujemny , ' + value)
+} else {
+    alert('Wynik = 0, ')
+}
+console.log(value);
+*/
+
+//Extra Task for Paweł
+/*
+var answer = prompt ('Ile wynosi 2+2?', '');
+
+if (answer == '4') {
+    alert('Jesteś genialny!')
+} else if (answer == '3' || answer =='5') {
+    alert('Spróbuj jeszcze raz')
+} else {
+    alert('Wróć do szkoły podstawowej i powtórz wiadomości!')
+}
+console.log(answer);
 */
