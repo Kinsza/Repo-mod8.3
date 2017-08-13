5ddcee3a4e119a7bf588c392f6b8a594f984037e
Kinsza <code1@onet.pl>
2017-08-13 12:29:15 +0200

    Added new task

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 // scripts.js
 
+//New task - Mod 8.2
+
 var name = prompt('Enter your name');
 // Learns to insert a new comment
 alert('Hello, ' + name);
 // Everything works so no more comments are needed
 console.log('Hello, ' + name);
+
+//You want to check the next task, remove '/*' '*/'
+
+//New task - Mod 8.5
+/*
+var a = prompt ('a');
+var h = prompt ('h');
+var triangleArea = a*h/2;
+
+alert('wynik, ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);*/
